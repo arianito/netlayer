d19a8965348d05f377d034c82c8bf665e05392a1
Aryan <collaborativeroom@gmail.com>
2019-10-12 14:10:58 +0330

    scope class

--- src/http.ts
+++ src/http.ts
@@ -1,4 +1,4 @@
-import { match, MatchResult } from './uri';
+import {match, MatchResult} from './uri';
 
 export type HttpHeaders = { [key: string]: string };
 export type HttpDriver = (request: HttpRequest) => Promise<HttpResponse>;
@@ -6,11 +6,6 @@ export type HttpRequestMiddleware = (request: HttpRequest) => HttpRequest;
 export type HttpResponseMiddleware = (response: HttpResponse) => HttpResponse;
 export type HttpMethod = 'POST' | 'GET' | 'PUT' | 'DELETE';
 export type HttpProgressType = 'download' | 'upload' | 'any';
-export type HttpResponseHelper = {
-	setHeader: (key: string, value: string) => any;
-	setStatus: (code: number) => any;
-	send: (text: any) => any;
-};
 export type HttpRequest = {
 	baseUrl?: string;
 	url: string;
@@ -23,7 +18,6 @@ export type HttpRequest = {
 	progress?: (status: HttpProgressType, total: number, loaded: number) => any;
 	[key: string]: any;
 };
-
 export type HttpResponse<T = any> = {
 	status?: number;
 	statusText?: string;
@@ -33,8 +27,6 @@ export type HttpResponse<T = any> = {
 	headers?: HttpHeaders;
 	[key: string]: any;
 };
-
-
 export type HttpConfig = {
 	baseUrl: string;
 	timeout: number;
@@ -50,23 +42,6 @@ export interface HttpController {
 	handler: (request: HttpRequest, response: HttpMockResponse) => any;
 }
 
-let defaultDriver: HttpDriver = null;
-const requestMiddlewareChain: {name: string, middleware: HttpRequestMiddleware}[] = [];
-const responseMiddlewareChain: {name: string, middleware: HttpResponseMiddleware}[] = [];
-const mockedRoutes: HttpController[] = [];
-
-export const configuration: HttpConfig = {
-	baseUrl: '',
-	timeout: 3600,
-	logger: null,
-	method: 'POST',
-	withCredentials: false,
-};
-
-export const config = (driver: HttpDriver) => {
-	defaultDriver = driver;
-};
-
 export class HttpMockResponse {
 	private context: any = null;
 
@@ -81,6 +56,7 @@ export class HttpMockResponse {
 	public write(data: string) {
 		this.context.body += data;
 	};
+
 	public set body(data: string) {
 		this.context.body = data;
 	};
@@ -90,164 +66,184 @@ export class HttpMockResponse {
 		this.context.body = JSON.stringify(data);
 	};
 
-	header(key: string, value: string){
+	header(key: string, value: string) {
 		this.context.headers[key] = value;
 	};
 }
 
-export function mock(path:string, method: HttpMethod = 'POST') {
-	return (target: any, key: string) => {
-		mockedRoutes.push({
-			path,
-			method,
-			handler: target[key],
-		})
+export class NetLayer {
+	driver: HttpDriver = null;
+	requestMiddlewareChain: { name: string, middleware: HttpRequestMiddleware }[] = [];
+	responseMiddlewareChain: { name: string, middleware: HttpResponseMiddleware }[] = [];
+	mockedRoutes: HttpController[] = [];
+	configuration: HttpConfig = {
+		baseUrl: '',
+		timeout: 3600,
+		logger: null,
+		method: 'POST',
+		withCredentials: false,
 	};
-}
 
-export function requestMiddleware(name: string, middleware: HttpRequestMiddleware) {
-	const index = requestMiddlewareChain.findIndex(a=>a.name == name);
-	if(index > -1){
-		if(middleware) {
-			requestMiddlewareChain[index].middleware = middleware;
-		}else {
-			requestMiddlewareChain.splice(index, 1);
-		}
-	}else {
-		requestMiddlewareChain.push({
-			name,
-			middleware,
-		});
+	constructor(driver: HttpDriver) {
+		this.driver = driver;
 	}
-}
-export function responseMiddleware(name: string, middleware: HttpResponseMiddleware) {
-	const index = requestMiddlewareChain.findIndex(a=>a.name == name);
-	if(index > -1){
-		if(middleware) {
-			responseMiddlewareChain[index].middleware = middleware;
-		}else {
-			responseMiddlewareChain.splice(index, 1);
-		}
-	}else {
-		responseMiddlewareChain.push({
-			name,
-			middleware,
-		});
-	}
-}
 
-export const request = async <R = any>(
-	request: HttpRequest,
-	driver: HttpDriver = null,
-): Promise<HttpResponse<R>> => {
-	let requestCache = request;
-	for (const m of requestMiddlewareChain) {
-		requestCache = m.middleware(requestCache);
-	}
-	//
-	let responseCache = null;
-	try {
-		responseCache = await (driver || defaultDriver)(requestCache);
-	} catch (e) {
-		let failureCache = e as HttpResponse;
-		for (const m of responseMiddlewareChain) {
-			failureCache = m.middleware(failureCache);
-		}
-		throw failureCache;
-	}
-	//
-	for (const m of responseMiddlewareChain) {
-		responseCache = m.middleware(responseCache);
-	}
-	return responseCache;
-};
-
-export const mockDriver = (fallback?: HttpDriver) => {
-	return async (request: HttpRequest): Promise<HttpResponse> => {
-		const logger = configuration.logger;
-
-		function log(...args: any[]) {
-			logger && logger(...args);
-		}
-		const path = (request.baseUrl || configuration.baseUrl) + request.url;
-		const method = request.method || configuration.method;
-		const oldPath = request.url;
-		const oldMethod = request.method;
-		request.url = path;
-		request.method = method;
-
-		log('request', request);
-
-		let methodNotFound = false;
-		for (const route of mockedRoutes) {
-			const matchedPath: MatchResult = match(path, {
-				path: configuration.baseHref + route.path,
-				exact: true,
-				sensitive: true,
-				strict: true,
-			});
-			if (matchedPath) {
-				if (route.method === method) {
-					const output: HttpResponse = {
-						headers: {},
-						status: 200,
-						body: '',
-					};
-					let isError = false;
-					try {
-						request.context = request.context || {};
-						request.context.match =matchedPath;
-						const res = new HttpMockResponse(output);
-
-						await route.handler(request, res);
-
-						log('response', output);
-
-						if (output.headers['Content-Type'] == 'application/json') {
-							try {
-								output.payload = JSON.parse(output.payload || '{}');
-							} catch (e) {
-								throw output;
-							}
-						}
-						if (output.status >= 400) {
-							isError = true;
-						} else {
-							return output;
-						}
-					} catch (e) {
-						log('error', '500 internal server error');
-						throw <HttpResponse>{
-							status: 500,
-							statusText: '500 internal server error',
-							payload: e,
-						};
-					}
-					throw output;
-				} else {
-					methodNotFound = true;
-				}
-			}
-		}
-		if (methodNotFound) {
-			log('error', '405 method not allowed');
-			throw <HttpResponse>{
-				status: 405,
-				statusText: '405 method not allowed',
-			};
-		}
-
-		if (fallback) {
-			log('fallback', oldPath);
-			request.url = oldPath;
-			request.method = oldMethod;
-			return fallback(request);
-		}
-
-		log('error', '404 not found');
-		throw <HttpResponse>{
-			status: 404,
-			statusText: '404 not found',
+	mock = (path: string, method: HttpMethod = 'POST') => {
+		return (target: any, key: string) => {
+			this.mockedRoutes.push({
+				path,
+				method,
+				handler: target[key],
+			})
 		};
 	};
-};
+
+	mockDriver = (fallback?: HttpDriver) => {
+		return async (request: HttpRequest): Promise<HttpResponse> => {
+			const logger = this.configuration.logger;
+
+			function log(...args: any[]) {
+				logger && logger(...args);
+			}
+
+			const path = (request.baseUrl || this.configuration.baseUrl) + request.url;
+			const method = request.method || this.configuration.method;
+			const oldPath = request.url;
+			const oldMethod = request.method;
+			request.url = path;
+			request.method = method;
+
+			log('request', request);
+
+			let methodNotFound = false;
+			for (const route of this.mockedRoutes) {
+				const matchedPath: MatchResult = match(path, {
+					path: this.configuration.baseUrl + route.path,
+					exact: true,
+					sensitive: true,
+					strict: true,
+				});
+				if (matchedPath) {
+					if (route.method === method) {
+						const output: HttpResponse = {
+							headers: {},
+							status: 200,
+							body: '',
+						};
+						let isError = false;
+						try {
+							request.context = request.context || {};
+							request.context.match = matchedPath;
+							const res = new HttpMockResponse(output);
+
+							await route.handler(request, res);
+
+							log('response', output);
+
+							if (output.headers['Content-Type'] == 'application/json') {
+								try {
+									output.payload = JSON.parse(output.payload || '{}');
+								} catch (e) {
+									throw output;
+								}
+							}
+							if (output.status >= 400) {
+								isError = true;
+							} else {
+								return output;
+							}
+						} catch (e) {
+							log('error', '500 internal server error');
+							throw <HttpResponse>{
+								status: 500,
+								statusText: '500 internal server error',
+								payload: e,
+							};
+						}
+						throw output;
+					} else {
+						methodNotFound = true;
+					}
+				}
+			}
+			if (methodNotFound) {
+				log('error', '405 method not allowed');
+				throw <HttpResponse>{
+					status: 405,
+					statusText: '405 method not allowed',
+				};
+			}
+
+			if (fallback) {
+				log('fallback', oldPath);
+				request.url = oldPath;
+				request.method = oldMethod;
+				return fallback(request);
+			}
+
+			log('error', '404 not found');
+			throw <HttpResponse>{
+				status: 404,
+				statusText: '404 not found',
+			};
+		};
+	};
+
+	requestMiddleware = (name: string, middleware: HttpRequestMiddleware) =>{
+		const index = this.requestMiddlewareChain.findIndex(a => a.name == name);
+		if (index > -1) {
+			if (middleware) {
+				this.requestMiddlewareChain[index].middleware = middleware;
+			} else {
+				this.requestMiddlewareChain.splice(index, 1);
+			}
+		} else {
+			this.requestMiddlewareChain.push({
+				name,
+				middleware,
+			});
+		}
+	};
+
+	responseMiddleware = (name: string, middleware: HttpResponseMiddleware) => {
+		const index = this.responseMiddlewareChain.findIndex(a => a.name == name);
+		if (index > -1) {
+			if (middleware) {
+				this.responseMiddlewareChain[index].middleware = middleware;
+			} else {
+				this.responseMiddlewareChain.splice(index, 1);
+			}
+		} else {
+			this.responseMiddlewareChain.push({
+				name,
+				middleware,
+			});
+		}
+	};
+
+	request = async <R = any>(
+		request: HttpRequest,
+		driver: HttpDriver = null,
+	): Promise<HttpResponse<R>> => {
+		let requestCache = request;
+		for (const m of this.requestMiddlewareChain) {
+			requestCache = m.middleware(requestCache);
+		}
+		//
+		let responseCache = null;
+		try {
+			responseCache = await (driver || this.driver)(requestCache);
+		} catch (e) {
+			let failureCache = e as HttpResponse;
+			for (const m of this.responseMiddlewareChain) {
+				failureCache = m.middleware(failureCache);
+			}
+			throw failureCache;
+		}
+		//
+		for (const m of this.responseMiddlewareChain) {
+			responseCache = m.middleware(responseCache);
+		}
+		return responseCache;
+	};
+}
